ddef5ca9cfd1218b839b54eecca75228e957729f
Kenni Bawden <kenni.bawden@gmail.com>
2014-07-31 19:31:05 +1000

    Added maps page and templates
    Maps are just JSON objects with a title at the moment

--- app/js/coursestitch.js
+++ app/js/coursestitch.js
@@ -1,1 +1,13 @@
-angular.module('coursestitch', []);
+angular.module('coursestitch', []).
+value('maps', [{
+    title: "Learn to Program",
+}, {
+    title: "Program an Arduino",
+}, {
+    title: "Create a website",
+}, {
+    title: "Make an App",
+}]).
+controller('mapsCtrl', function($scope, maps) {
+    $scope.maps = maps;
+});
