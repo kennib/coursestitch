946d93c41aa08994b00fbae88e4e0bf1eba31fe3
Kenni Bawden <kenni.bawden@gmail.com>
2014-07-31 21:13:51 +1000

    Added map page and resource viewer
    Includes resource summary and title
    still needs resource tags added

--- app/js/coursestitch.js
+++ app/js/coursestitch.js
@@ -8,6 +8,20 @@ value('maps', [{
 }, {
     title: "Make an App",
 }]).
+value('map', {
+    title: "Learn to program",
+    resources: [{
+        title: "Angry Birds Hour of Code",
+        subtitle: "Level 1",
+        url: "http://learn.code.org/hoc/1",
+        image: "http://learn.code.org/blockly/media/skins/birds/small_static_avatar.png",
+        summary: "Can you help me to catch the naughty pig? Stack a couple of \"move forward\" blocks together and press \"Run\" to help me get there.",
+    }],
+}).
 controller('mapsCtrl', function($scope, maps) {
     $scope.maps = maps;
+}).
+controller('viewerCtrl', function($scope, map) {
+    $scope.map = map;
+    $scope.resource = map.resources[0];
 });
